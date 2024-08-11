8731e105904201b0d3488c553303357d5432f303
Nishanth Prem <nishanthprem8@gmail.com>
2024-08-10 21:07:13 -0700

    Adding more routes.

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,18 @@
 use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
+async fn home() -> impl Responder {
+    HttpResponse::Ok().body("Home Page")
+}
+
+#[post("/login")]
+async fn login() -> impl Responder {
+    HttpResponse::Ok().body("Login")
+}
+
+#[get("/logout")]
+async fn logout() -> impl Responder {
+    HttpResponse::Ok().body("Logout")
 }
 
 #[post("/echo")]
@@ -10,17 +20,13 @@ async fn echo(req_body: String) -> impl Responder {
     HttpResponse::Ok().body(req_body)
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
-
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
-            .service(hello)
+            .service(web::scope("/auth").service(login).service(logout))
+            .service(home)
             .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
     .bind(("127.0.0.1", 8080))?
     .run()
