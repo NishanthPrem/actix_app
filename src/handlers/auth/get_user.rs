53d3db759e88e8b4483b7035b49e63181668457f
Nishanth Prem <nishanthprem8@gmail.com>
2024-08-20 22:09:50 -0700

    Removing unnecessary imports.

--- src/handlers/auth/get_user.rs
+++ src/handlers/auth/get_user.rs
@@ -1,5 +1,5 @@
 use crate::models::{auth, errors};
-use actix_web::{get, web, HttpResponse, Responder};
+use actix_web::{get, web, Responder};
 
 #[get("/user")]
 async fn get_user(path: web::Query<auth::PathParameters>) -> Result<impl Responder, errors::HttpError> {
